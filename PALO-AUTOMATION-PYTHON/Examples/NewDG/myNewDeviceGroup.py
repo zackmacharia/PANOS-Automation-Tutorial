3e2382b283fb66674f2f9c220effc44b3c3190d9
Zack Macharia <blaqhrse@gmail.com>
2022-01-29 18:42:58 -0600

    updated code comments

--- PALO-AUTOMATION-PYTHON/Examples/NewDG/myNewDeviceGroup.py
+++ PALO-AUTOMATION-PYTHON/Examples/NewDG/myNewDeviceGroup.py
@@ -1,6 +1,26 @@
-import os
-from panos import panorama
+import os # This package allows the script to interact with the computer operating system
+from panos import panorama # This is the panorama module from pan-os-python library
 
+"""
+For security reasons instead of hardcoding the API Key (credentials) on the script. It's better to use environmental variables.
+Below are the steps I used to create envrionmental variables in MACOS
+Open Terminal
+
+`nano ~/.bash_profile`
+export PANORAMA="PANORAMAFQDN_OR_IPADDRESS"
+export PANOAPIKEY="APIKEYVALUEHERE"
+:q!
+ctrl+0
+ctrl+X
+
+Restart the session either by closing out the terminal or running the source command. 
+"source ~/.bash_profile"
+Press Enter
+
+Verify the environmental variables by running:
+
+"printenv | grep PANO"
+"""
 DEVICE = os.getenv("PANORAMA")
 APIKEY = os.getenv("PANOAPIKEY")
 
